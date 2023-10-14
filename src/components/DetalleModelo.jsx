f5b48330817fde9a0d30e723a82989b9ac57ef70
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2023-10-13 21:17:35 -0500

    se eliminaron las cajas de descripcion

--- src/components/DetalleModelo.jsx
+++ src/components/DetalleModelo.jsx
@@ -61,23 +61,6 @@ export const DetalleModelo = () => {
         </div>
       </section>
 
-      <div class="boxes">
-        <div class="img-box">
-          <img src="https://www.mazdavardi.com.co/wp-content/uploads/2023/08/mazda-cx-90-ficha-tecnica.webp" alt="Mazda CX-90" class="img-parallax" />
-        </div>
-        <div class="description-box">
-          <div class="box-text">
-            <h1>MAZDA CX-90</h1>
-            <br />
-            <p>LA MAESTRÍA SE CONDUCE</p>
-            <p>La nueva Mazda CX-90 es el resultado de la pasión y más alta precisión de la maestría japonesa unidas para esculpir una obra de arte e ingeniería. Su diseño audaz y dinámico ponen en perfecto balance el desempeño y confort, gracias a su motor eSkyactiv-G turbo de 6 cilindros en línea, con tecnología Mild Hybrid, convirtiendo a esta SUV de 7 puestos en la más poderosa y elegante de la marca.</p>
-            <div class="links">
-              <a href="https://www.mazdavardi.com.co/wp-content/uploads/2023/08/ficha-tecnica-mazda-cx-90.pdf" download="" class="btn"><span>FICHA TÉCNICA <i class="lni lni-book"></i></span></a>
-            </div>
-          </div>
-        </div>
-      </div>
-
     </div>
   )
 }
