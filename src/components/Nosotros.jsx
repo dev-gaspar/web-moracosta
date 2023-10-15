b94907d9e4ec64fbd1521386b1ae4d73101b425a
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2023-10-15 09:39:29 -0500

    vista nosotros terminada

--- src/components/Nosotros.jsx
+++ src/components/Nosotros.jsx
@@ -1,35 +1,83 @@
 const Nosotros = () => {
   return (
-    <div>
+    <>
       <div className="top-fixed" />
-      <div class="container">
-        <header class="text-center my-5">
-          <h1>Quiénes Somos</h1>
-        </header>
-        <section>
-          <div class="row">
-            <div class="col-lg-6">
-              <h2>Nuestra Historia</h2>
-              <p>Tu empresa es una organización comprometida con...</p>
-            </div>
-            <div class="col-lg-6">
-              <h2>Nuestro Equipo</h2>
-              <p>Estamos formados por un equipo de profesionales...</p>
+      <section className="company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding" id="section">
+        <div className="container text-center two-content">
+          <h2>HISTORIA</h2>
+          <h3>Grupo Moracosta</h3>
+          <div className="col-md-8 col-md-offset-2">
+            <div className="red-border"></div>
+            <p className="ct-u-size22 ct-u-fontWeight300 marginTop40">
+              NEGOCIOS MORACOSTA S.A. tiene 20 años en el mercado Manabita representando marcas de excelente prestigio, rendimiento y estilo automotriz; iniciamos actividades en el año 2003 y fuimos representante de la marca SKODA hasta el año 2010 y luego en este mismo año asumimos la distribución de las marcas CHERY y DONGFENG hasta la presente fecha.<br />
+              <br />
+              El fin de la empresa es la compra y venta de vehículos, repuestos así como dar la cobertura de servicios para todo el parque automotriz que existe en la zona y el que se vaya creando como consecuencia de las ventas
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="culture-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding">
+        <div className="container">
+          <div className="row two-content ">
+            <div className="col-md-8 col-md-offset-2">
+              <h2>Nuestros valores</h2>
+              <p className="ct-u-size22 ct-u-fontWeight300 ct-u-marginBottom60">
+                En nuestra institución, nos enorgullece operar diariamente con los siguientes principios:
+              </p>
             </div>
           </div>
-        </section>
-
-        <section class="my-5">
-          <h1>Misión</h1>
-          <p>Nuestra misión es...</p>
-        </section>
-
-        <section class="my-5">
-          <h1>Visión</h1>
-          <p>Nuestra visión es...</p>
-        </section>
-      </div>
-    </div>
+          <div className="row ct-u-paddingBoth20">
+            <div className="col-xs-6 col-md-4">
+              <div className="company-icons-white">
+                <i class="fas fa-handshake"></i>
+                <p>LEALTAD</p>
+                <p className="company-icons-subtext hidden-xs">
+                  La lealtad es un pilar fundamental. Nos comprometemos a ser leales a nuestros clientes, empleados y a los principios en los que se basa nuestra empresa.
+                </p>
+              </div>
+            </div>
+            <div className="col-xs-6 col-md-4">
+              <div className="company-icons-white">
+                <i class="fas fa-balance-scale"></i>
+                <p>HONESTIDAD</p>
+                <p className="company-icons-subtext hidden-xs">
+                  La honestidad guía cada una de nuestras acciones diarias. Mantenemos la transparencia en todos nuestros tratos y relaciones comerciales, creando así un ambiente de confianza.
+                </p>
+              </div>
+            </div>
+            <div className="col-xs-6 col-md-4">
+              <div className="company-icons-white">
+                <i class="fas fa-star"></i>
+                <p>CALIDAD</p>
+                <p className="company-icons-subtext hidden-xs">
+                  La calidad es esencial en todo lo que hacemos. Nos esforzamos por ofrecer productos y servicios de la más alta calidad para garantizar la satisfacción y la confianza de nuestros clientes.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding" id="section">
+        <div className="container text-center two-content">
+          <h2>VISION</h2>
+          <div className="col-md-8 col-md-offset-2">
+            <p className="ct-u-size22 ct-u-fontWeight300 marginTop40">
+              Comercializar automotores, sus partes y dar servicios de mantenimientos para el sector AUTOMOTRIZ con el más alto nivel de exigencia.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding" id="section">
+        <div className="container text-center two-content">
+          <h2>MISION</h2>
+          <div className="col-md-8 col-md-offset-2">
+            <p className="ct-u-size22 ct-u-fontWeight300 marginTop40">
+              Otorgar al Cliente un servicio de Calidad y Eficiencia que supere las expectativas, llevadas por un excelente ambiente laboral liderado por los ejecutivos y accionistas de la empresa.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
