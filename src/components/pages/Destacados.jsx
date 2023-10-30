c66ead77a42f2357dbab58686ab61ccec4545589
Jose Gaspar <jgasparlopez29@correo.unicordoba.edu.co>
2023-10-30 15:56:35 -0500

    mapeo de vehiculos destacados

--- src/components/pages/Destacados.jsx
+++ src/components/pages/Destacados.jsx
@@ -1,6 +1,56 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
+import { getVehiculos, getVehiculosError, getVehiculosStatus, selectAllVehiculos } from "../../features/vehiculos/vehiculosSlice"
 
 const Destacados = () => {
+
+  const dispatch = useDispatch()
+  const vehiculos = useSelector(selectAllVehiculos)
+  const status = useSelector(getVehiculosStatus)
+  const error = useSelector(getVehiculosError)
+
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(getVehiculos())
+    }
+  }, [status, dispatch])
+
+  let contenido;
+
+  if (status === 'loading') {
+    contenido = <div className="w-100 text-center">
+      <div className="spinner-border text-danger" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
+  } else if (status === 'succeeded') {
+
+    //obtener los ultimos vehiculos registrados
+    let vehiculosDestacados = [...vehiculos].reverse().slice(0, 3)
+
+    contenido =
+      vehiculosDestacados.map(vehiculo => (
+        <div className="col-12 col-lg-4">
+          <div className="box-car">
+            <img src={vehiculo.imagen_principal.url} />
+            <h2>{vehiculo.nombre}</h2>
+            <div className="box-text">
+              <p>
+                {vehiculo.descripcion}
+              </p>
+              <Link to={`/modelos/${vehiculo._id}`} className="cta">CONÓCELO</Link>
+            </div>
+          </div>
+        </div>
+      ))
+  } else if (status === 'failed') {
+    contenido =
+      <div className="alert alert-danger" role="alert">
+        {error}
+      </div>
+  }
+
   return (
     <div className="destacados p-5 mt-3">
       <div className="container text-center">
@@ -8,44 +58,7 @@ const Destacados = () => {
         <h2>Vehículos destacados</h2>
         <br />
         <div className="row">
-          <div className="col-12 col-lg-4">
-            <div className="box-car">
-              <img src="../assets/carro1.webp" />
-              <h2>Mazda MX-30 EV</h2>
-              <div className="box-text">
-                <p>
-                  Conduce el cambio
-                </p>
-                <Link className="cta">CONÓCELO</Link>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-12 col-lg-4">
-            <div className="box-car">
-              <img src="../assets/carro2.webp" />
-              <h2>Mazda CX-50</h2>
-              <div className="box-text">
-                <p>
-                  Tan emo­cion­ante como su­perar un nuevo cam­ino
-                </p>
-                <Link className="cta">CONÓCELO</Link>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-12 col-lg-4">
-            <div className="box-car">
-              <img src="../assets/carro3.webp" />
-              <h2>Mazda 2 Sedán</h2>
-              <div className="box-text">
-                <p>
-                  El nuevo carro Mazda 2 Sedán llegó para sorprenderte ¡Conoce todas sus características y accesorios aquí!
-                </p>
-                <Link className="cta">CONÓCELO</Link>
-              </div>
-            </div>
-          </div>
+          {contenido}
         </div>
       </div>
     </div>
