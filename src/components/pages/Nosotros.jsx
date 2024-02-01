78dd7efbfb0a2b1fd0d1ee932353e55bf348c395
Sanx77 <98669630+dev-gaspar@users.noreply.github.com>
2024-02-01 12:54:23 -0500

    Agregadas secciones Nosotros y Valores en español

--- src/components/pages/Nosotros.jsx
+++ src/components/pages/Nosotros.jsx
@@ -9,20 +9,27 @@ const Nosotros = () => {
           <div className="col-md-8 col-md-offset-2">
             <div className="red-border"></div>
             <p className="ct-u-size22 ct-u-fontWeight300 marginTop40">
-              NEGOCIOS MORACOSTA S.A. tiene 20 años en el mercado Manabita representando marcas de excelente prestigio, rendimiento y estilo automotriz; iniciamos actividades en el año 2003 y fuimos representante de la marca SKODA hasta el año 2010 y luego en este mismo año asumimos la distribución de las marcas CHERY y DONGFENG hasta la presente fecha.<br />
-              <br />
-              El fin de la empresa es la compra y venta de vehículos, repuestos así como dar la cobertura de servicios para todo el parque automotriz que existe en la zona y el que se vaya creando como consecuencia de las ventas
+              Contamos con 20 años de experiencia y excelencia en el corazón de Manabí. Desde nuestro inicio en 2003, nos enorgullece haber representado marcas de renombre con prestigio, rendimiento y estilo incomparables en el mercado.
+            </p>
+            <p className="ct-u-size22 ct-u-fontWeight300 marginTop40">
+              En el año 2010, decidimos expandir nuestra oferta para brindar a los manabitas aún más opciones de calidad, asumiendo la distribución de las marcas CHERY y DONGFENG.
+            </p>
+            <p className="ct-u-size22 ct-u-fontWeight300 marginTop40">
+              En NEGOCIOS MORACOSTA S.A., no solo nos dedicamos a vender automóviles, sino que nos esforzamos por ofrecer una experiencia completa a nuestros clientes. Nuestro equipo está comprometido con la satisfacción del cliente, proporcionando asesoramiento experto, servicios de mantenimiento de alta calidad y opciones de financiamiento flexibles.
+            </p>
+            <p className="ct-u-size22 ct-u-fontWeight300 marginTop40">
+              Durante todo este tiempo hemos construido una reputación sólida en el mercado manabita basada en la integridad y el compromiso con la excelencia. Agradecemos a nuestros clientes por su lealtad y confianza a lo largo de los años.
             </p>
           </div>
         </div>
-      </section>
+      </section >
       <section className="culture-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding">
         <div className="container">
           <div className="row two-content ">
             <div className="col-md-8 col-md-offset-2">
               <h2>Nuestros valores</h2>
               <p className="ct-u-size22 ct-u-fontWeight300 ct-u-marginBottom60">
-                En nuestra institución, nos enorgullece operar diariamente con los siguientes principios:
+                En NEGOCIOS MORACOSTA S.A., nos regimos por un conjunto de valores fundamentales que guían nuestras acciones y definen nuestra cultura empresarial. Estos valores son la base de nuestra filosofía y reflejan el compromiso con la excelencia en el servicio y la satisfacción del cliente:
               </p>
             </div>
           </div>
@@ -32,7 +39,7 @@ const Nosotros = () => {
                 <i className="fas fa-handshake"></i>
                 <p>LEALTAD</p>
                 <p className="company-icons-subtext hidden-xs">
-                  La lealtad es un pilar fundamental. Nos comprometemos a ser leales a nuestros clientes, empleados y a los principios en los que se basa nuestra empresa.
+                  Fomentamos la lealtad en todas las relaciones, ya sea con nuestros clientes, colaboradores o socios comerciales. Valoramos la confianza depositada en nosotros y trabajamos constantemente para fortalecer esos lazos a lo largo del tiempo.
                 </p>
               </div>
             </div>
@@ -41,7 +48,7 @@ const Nosotros = () => {
                 <i className="fas fa-balance-scale"></i>
                 <p>HONESTIDAD</p>
                 <p className="company-icons-subtext hidden-xs">
-                  La honestidad guía cada una de nuestras acciones diarias. Mantenemos la transparencia en todos nuestros tratos y relaciones comerciales, creando así un ambiente de confianza.
+                  La transparencia y la honestidad son esenciales en todas nuestras interacciones. Nos comprometemos a actuar con integridad en cada paso, proporcionando información clara y precisa, y siendo honestos en todas nuestras transacciones.
                 </p>
               </div>
             </div>
@@ -50,29 +57,30 @@ const Nosotros = () => {
                 <i className="fas fa-star"></i>
                 <p>CALIDAD</p>
                 <p className="company-icons-subtext hidden-xs">
-                  La calidad es esencial en todo lo que hacemos. Nos esforzamos por ofrecer productos y servicios de la más alta calidad para garantizar la satisfacción y la confianza de nuestros clientes.
+                  Buscamos la excelencia en todos los servicios que brindamos, siendo muy selectivos en las marcas que representamos porque la calidad no es solo un estándar, sino un compromiso constante.
                 </p>
               </div>
             </div>
           </div>
         </div>
       </section>
+
       <section className="company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding" id="section">
         <div className="container text-center two-content">
-          <h2>VISION</h2>
+          <h2>MISIÓN</h2>
           <div className="col-md-8 col-md-offset-2">
             <p className="ct-u-size22 ct-u-fontWeight300 marginTop40">
-              Comercializar automotores, sus partes y dar servicios de mantenimientos para el sector AUTOMOTRIZ con el más alto nivel de exigencia.
+              Nuestra misión es proporcionar a nuestros clientes una experiencia automotriz excepcional, ofreciendo vehículos de calidad respaldados por un servicio de excelencia. Nos dedicamos a satisfacer las necesidades de movilidad de nuestros clientes, brindando opciones diversificadas y soluciones personalizadas. A través de nuestra pasión por la innovación y el compromiso con la lealtad, honestidad y calidad, buscamos ser el referente indiscutible en el mercado automotriz de Manabí.
             </p>
           </div>
         </div>
       </section>
       <section className="company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding" id="section">
         <div className="container text-center two-content">
-          <h2>MISION</h2>
+          <h2>VISIÓN</h2>
           <div className="col-md-8 col-md-offset-2">
             <p className="ct-u-size22 ct-u-fontWeight300 marginTop40">
-              Otorgar al Cliente un servicio de Calidad y Eficiencia que supere las expectativas, llevadas por un excelente ambiente laboral liderado por los ejecutivos y accionistas de la empresa.
+              Nos visualizamos como líderes indiscutibles en la industria automotriz de Manabí, reconocidos por la excelencia en productos y servicios. Buscamos constantemente expandir y diversificar nuestra oferta, incorporando las últimas innovaciones y tecnologías para anticiparnos a las necesidades cambiantes del mercado. Aspiramos a ser un referente en la satisfacción del cliente, manteniendo relaciones a largo plazo basadas en la confianza y el compromiso. Con un equipo altamente capacitado y una cultura de mejora continua, perseguimos el crecimiento sostenible y la contribución positiva al desarrollo de la comunidad y la economía local.
             </p>
           </div>
         </div>
